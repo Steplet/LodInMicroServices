fbd1a26546abb0173b22dc2287f89dd53250e956
Artem <asteberg@list.ru>
2023-12-17 17:44:15 +0300

    Initial commit

---
+++ src/test/kotlin/com/example/LodInMicroServices/LodInMicroServicesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.LodInMicroServices
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LodInMicroServicesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
